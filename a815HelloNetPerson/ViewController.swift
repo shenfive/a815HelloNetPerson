9c88bca7ec01fb36cac039ca32815a9624f938dd
Danny Shen <shenfive@hotmail.com>
2021-08-15 16:08:25 +0800

    Initial Commit

---
+++ a815HelloNetPerson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a815HelloNetPerson
+//
+//  Created by 申潤五 on 2021/8/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
